1e1034b18ac369e762fe40d14c45a8ef6af3b11b
loceme <loceme.student@163.com>
2018-05-07 08:48:04 +0800

    分布式自增id组件

--- simba-autoId/src/main/java/com/simba/service/impl/AutoIdServiceImpl.java
+++ simba-autoId/src/main/java/com/simba/service/impl/AutoIdServiceImpl.java
@@ -1,9 +1,11 @@
 package com.simba.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import com.simba.cache.Redis;
@@ -19,23 +21,32 @@ import com.simba.service.AutoIdService;
 @Service
 public class AutoIdServiceImpl implements AutoIdService {
 
+	private static final String suffix = "_autoId";
+
 	@Resource
 	private Redis redisUtil;
 
 	@Override
 	public void batchDelete(List<String> asList) {
-
+		asList.forEach((id) -> {
+			this.delete(id);
+		});
 	}
 
 	@Override
 	public void delete(String id) {
-
+		String key = id + suffix;
+		redisUtil.removeString(key);
 	}
 
 	@Override
 	public List<AutoId> listAll(String key) {
+		key = StringUtils.defaultString(key, StringUtils.EMPTY);
+		String keys = key + "*" + suffix;
+		List<String> keyList = redisUtil.keysString(keys);
+		List<AutoId> autoIdList = new ArrayList<>(keyList.size());
 
-		return null;
+		return autoIdList;
 	}
 
 	@Override

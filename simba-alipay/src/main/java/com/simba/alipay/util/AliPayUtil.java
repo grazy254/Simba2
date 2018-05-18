2e16ec4681b3fe7a9f3954d292318c79f870d6d2
loceme <loceme.student@163.com>
2018-05-18 10:17:49 +0800

    阿里支付

--- simba-alipay/src/main/java/com/simba/alipay/util/AliPayUtil.java
+++ simba-alipay/src/main/java/com/simba/alipay/util/AliPayUtil.java
@@ -1,5 +1,7 @@
 package com.simba.alipay.util;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,13 +14,19 @@ import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.request.AlipayDataDataserviceBillDownloadurlQueryRequest;
+import com.alipay.api.request.AlipayTradeFastpayRefundQueryRequest;
+import com.alipay.api.request.AlipayTradeOrderSettleRequest;
 import com.alipay.api.request.AlipayTradeQueryRequest;
 import com.alipay.api.request.AlipayTradeRefundRequest;
 import com.alipay.api.response.AlipayDataDataserviceBillDownloadurlQueryResponse;
+import com.alipay.api.response.AlipayTradeFastpayRefundQueryResponse;
+import com.alipay.api.response.AlipayTradeOrderSettleResponse;
 import com.alipay.api.response.AlipayTradeQueryResponse;
 import com.alipay.api.response.AlipayTradeRefundResponse;
 import com.simba.alipay.cosntantData.AliPayConstantData;
+import com.simba.alipay.model.RoyaltyParameter;
 import com.simba.exception.BussException;
+import com.simba.framework.util.json.FastJsonUtil;
 import com.simba.model.constant.ConstantData;
 
 /**
@@ -110,6 +118,51 @@ public class AliPayUtil {
 		request.setBizContent("{" + "    \"bill_type\":\"trade\"," + "    \"bill_date\":\"" + bill_date + "\"" + "  }");// 设置业务参数
 		AlipayDataDataserviceBillDownloadurlQueryResponse response = alipayClient.execute(request);
 		logger.info("查询对账单下载地址返回结果:" + response.getBody());
+		return response;
+	}
+
+	/**
+	 * 统一收单交易退款查询
+	 * 
+	 * @param trade_no
+	 *            支付宝交易号，和商户订单号不能同时为空
+	 * @param out_trade_no
+	 *            订单支付时传入的商户订单号,和支付宝交易号不能同时为空。
+	 *            trade_no,out_trade_no如果同时存在优先取trade_no
+	 * @param out_request_no
+	 *            请求退款接口时，传入的退款请求号，如果在退款请求时未传入，则该值为创建交易时的外部交易号
+	 * @return
+	 * @throws AlipayApiException
+	 */
+	public AlipayTradeFastpayRefundQueryResponse refundQuery(String trade_no, String out_trade_no, String out_request_no) throws AlipayApiException {
+		AlipayTradeFastpayRefundQueryRequest request = new AlipayTradeFastpayRefundQueryRequest();
+		request.setBizContent("{" + "\"trade_no\":\"" + StringUtils.defaultString(trade_no) + "\"," + "\"out_trade_no\":\"" + StringUtils.defaultString(out_trade_no) + "\"," + "\"out_request_no\":\""
+				+ StringUtils.defaultString(out_request_no) + "\"" + "  }");
+		AlipayTradeFastpayRefundQueryResponse response = alipayClient.execute(request);
+		logger.info("统一收单交易退款查询返回结果:" + response.getBody());
+		return response;
+	}
+
+	/**
+	 * 统一收单交易结算
+	 * 
+	 * @param out_request_no
+	 *            结算请求流水号 开发者自行生成并保证唯一性
+	 * @param trade_no
+	 *            支付宝订单号
+	 * @param royalty_parameters
+	 *            分账明细信息
+	 * @param operator_id
+	 *            操作员id
+	 * @return
+	 * @throws AlipayApiException
+	 */
+	public AlipayTradeOrderSettleResponse orderSettle(String out_request_no, String trade_no, List<RoyaltyParameter> royalty_parameters, String operator_id) throws AlipayApiException {
+		AlipayTradeOrderSettleRequest request = new AlipayTradeOrderSettleRequest();
+		request.setBizContent("{" + "\"out_request_no\":\"" + StringUtils.defaultString(out_request_no) + "\"," + "\"trade_no\":\"" + StringUtils.defaultString(trade_no) + "\","
+				+ "      \"royalty_parameters\":" + FastJsonUtil.toJson(royalty_parameters) + "," + "\"operator_id\":\"" + StringUtils.defaultString(operator_id) + "\"" + "  }");
+		AlipayTradeOrderSettleResponse response = alipayClient.execute(request);
+		logger.info("统一收单交易结算返回结果:" + response.getBody());
 		return response;
 	}
 

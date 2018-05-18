fb307dfd163f96904fb2015e32d34209154d79f4
loceme <loceme.student@163.com>
2018-05-18 13:49:23 +0800

    阿里支付

--- simba-wechat-pay-bill/src/main/java/com/simba/impls/PayBillImpl.java
+++ simba-wechat-pay-bill/src/main/java/com/simba/impls/PayBillImpl.java
@@ -1,10 +1,13 @@
 package com.simba.impls;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.simba.interfaces.PayInterface;
+import com.simba.model.PayBill;
 import com.simba.model.pay.result.PayResult;
 import com.simba.model.pay.unifiedorder.UnifiedOrderReq;
+import com.simba.service.PayBillService;
 
 /**
  * 处理微信支付账单
@@ -15,6 +18,9 @@ import com.simba.model.pay.unifiedorder.UnifiedOrderReq;
 @Component
 public class PayBillImpl implements PayInterface {
 
+	@Autowired
+	private PayBillService payBillService;
+
 	@Override
 	public void dealResult(PayResult payResult) {
 
@@ -22,7 +28,9 @@ public class PayBillImpl implements PayInterface {
 
 	@Override
 	public void dealOrder(UnifiedOrderReq req) {
+		PayBill bill = new PayBill();
 
+		payBillService.add(bill);
 	}
 
 }

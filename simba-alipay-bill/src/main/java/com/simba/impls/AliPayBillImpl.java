34d93bcaaeea626802190a991c3fc0791a17629c
loceme <loceme.student@163.com>
2018-05-25 17:01:10 +0800

    阿里支付账单

--- simba-alipay-bill/src/main/java/com/simba/impls/AliPayBillImpl.java
+++ simba-alipay-bill/src/main/java/com/simba/impls/AliPayBillImpl.java
@@ -9,7 +9,8 @@ import com.simba.alipay.controller.form.AliPayCloseForm;
 import com.simba.alipay.controller.form.AliPayRefundForm;
 import com.simba.alipay.controller.form.AppPayForm;
 import com.simba.alipay.interfaces.AliPayInterface;
-import com.simba.alipay.service.AliPayService;
+import com.simba.model.AliPayBill;
+import com.simba.service.AliPayBillService;
 
 /**
  * 阿里支付账单的实现类
@@ -21,7 +22,7 @@ import com.simba.alipay.service.AliPayService;
 public class AliPayBillImpl implements AliPayInterface {
 
 	@Autowired
-	private AliPayService aliPayService;
+	private AliPayBillService aliPayBillService;
 
 	@Override
 	public void dealCallback(AliPayCallbackForm callbackForm) {
@@ -30,7 +31,9 @@ public class AliPayBillImpl implements AliPayInterface {
 
 	@Override
 	public void appPay(AppPayForm payForm) {
+		AliPayBill bill = new AliPayBill();
 
+		aliPayBillService.add(bill);
 	}
 
 	@Override

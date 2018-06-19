36031ff037bdeea2639f1e79a4f15826431a6f58
loceme <loceme.student@163.com>
2018-06-19 09:49:48 +0800

    阿里支付账单

--- simba-alipay-bill/src/main/java/com/simba/jobs/AliPayPayJob.java
+++ simba-alipay-bill/src/main/java/com/simba/jobs/AliPayPayJob.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.w3c.dom.DOMException;
@@ -25,6 +27,9 @@ public class AliPayPayJob {
 	@Autowired
 	private AliPayBillService aliPayBillService;
 
+	@Value("${alipay.appid}")
+	private String appId;
+
 	/**
 	 * 查询未完成的订单，更新其状态
 	 * 
@@ -36,6 +41,8 @@ public class AliPayPayJob {
 	 */
 	@Scheduled(cron = "0 0/2 * * * *")
 	public void checkUnfinishOrder() throws DOMException, XPathExpressionException, ParserConfigurationException, SAXException, IOException {
-		aliPayBillService.checkUnfinishOrder();
+		if (StringUtils.isNotEmpty(appId)) {
+			aliPayBillService.checkUnfinishOrder();
+		}
 	}
 }
--- simba-wechat-pay-bill/src/main/java/com/simba/jobs/WechatPayJob.java
+++ simba-wechat-pay-bill/src/main/java/com/simba/jobs/WechatPayJob.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.w3c.dom.DOMException;
@@ -25,16 +27,22 @@ public class WechatPayJob {
 	@Autowired
 	private PayBillService payBillService;
 
+	@Value("${appID}")
+	private String appID;
+
 	/**
 	 * 查询未完成的订单，更新其状态
-	 * @throws IOException 
-	 * @throws SAXException 
-	 * @throws ParserConfigurationException 
-	 * @throws XPathExpressionException 
-	 * @throws DOMException 
+	 * 
+	 * @throws IOException
+	 * @throws SAXException
+	 * @throws ParserConfigurationException
+	 * @throws XPathExpressionException
+	 * @throws DOMException
 	 */
 	@Scheduled(cron = "0 0/2 * * * *")
 	public void checkUnfinishOrder() throws DOMException, XPathExpressionException, ParserConfigurationException, SAXException, IOException {
-		payBillService.checkUnfinishOrder();
+		if (StringUtils.isNotEmpty(appID)) {
+			payBillService.checkUnfinishOrder();
+		}
 	}
 }

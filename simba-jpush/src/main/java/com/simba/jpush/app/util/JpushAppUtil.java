cd46592d0e1e5d4d9020c19f0a215229e6d8d30f
shuoGG <328893769@qq.com>
2018-06-23 20:37:19 +0800

    增加消息推送

--- simba-jpush/src/main/java/com/simba/jpush/app/util/JpushAppUtil.java
+++ simba-jpush/src/main/java/com/simba/jpush/app/util/JpushAppUtil.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import cn.jpush.api.push.model.Message;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,90 +25,122 @@ import cn.jpush.api.push.model.notification.Notification;
 
 /**
  * jpush推送App工具类
- * 
- * @author caozhejun
  *
+ * @author caozhejun
  */
 @Component
 public class JpushAppUtil {
 
-	private static final Log logger = LogFactory.getLog(JpushAppUtil.class);
+    private static final Log logger = LogFactory.getLog(JpushAppUtil.class);
 
-	@Value("${jpush.key}")
-	private String key;
+    @Value("${jpush.key}")
+    private String key;
 
-	@Value("${jpush.secret}")
-	private String secret;
+    @Value("${jpush.secret}")
+    private String secret;
 
-	private JPushClient client;
+    private JPushClient client;
 
-	private boolean enabled = false;
+    private boolean enabled = false;
 
-	public JPushClient getClient() {
-		return client;
-	}
+    public JPushClient getClient() {
+        return client;
+    }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+    public boolean isEnabled() {
+        return enabled;
+    }
 
-	@PostConstruct
-	private void init() {
-		if (StringUtils.isEmpty(secret) || StringUtils.isEmpty(key)) {
-			return;
-		}
-		client = new JPushClient(secret, key, null, ClientConfig.getInstance());
-		enabled = true;
-	}
+    @PostConstruct
+    private void init() {
+        if (StringUtils.isEmpty(secret) || StringUtils.isEmpty(key)) {
+            return;
+        }
+        client = new JPushClient(secret, key, null, ClientConfig.getInstance());
+        enabled = true;
+    }
 
-	@PreDestroy
-	private void destroy() {
-		if (client != null) {
-			client.close();
-		}
-	}
+    @PreDestroy
+    private void destroy() {
+        if (client != null) {
+            client.close();
+        }
+    }
 
-	/**
-	 * 使用别名列表发送极光推送通知
-	 * 
-	 * @param users
-	 * @param content
-	 * @throws APIRequestException
-	 * @throws APIConnectionException
-	 */
-	public PushResult sendNotification(List<String> users, String content) throws APIConnectionException, APIRequestException {
-		PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.newBuilder().addAudienceTarget(AudienceTarget.alias(users)).build())
-				.setNotification(Notification.alert(content)).build();
-		PushResult result = sendPush(payload);
-		return result;
-	}
+    /**
+     * 使用别名列表发送极光推送通知
+     *
+     * @param users
+     * @param content
+     * @throws APIRequestException
+     * @throws APIConnectionException
+     */
+    public PushResult sendNotification(List<String> users, String content) throws APIConnectionException, APIRequestException {
+        PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.newBuilder().addAudienceTarget(AudienceTarget.alias(users)).build())
+                .setNotification(Notification.alert(content)).build();
+        PushResult result = sendPush(payload);
+        return result;
+    }
 
-	/**
-	 * 使用别名发送极光推送通知
-	 * 
-	 * @param user
-	 * @param content
-	 */
-	public PushResult sendNotification(String user, String content) throws APIConnectionException, APIRequestException {
-		PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.alias(user)).setNotification(Notification.alert(content)).build();
-		PushResult result = sendPush(payload);
-		return result;
-	}
+    /**
+     * 使用别名发送极光推送通知
+     *
+     * @param user
+     * @param content
+     */
+    public PushResult sendNotification(String user, String content) throws APIConnectionException, APIRequestException {
+        PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.alias(user)).setNotification(Notification.alert(content)).build();
+        PushResult result = sendPush(payload);
+        return result;
+    }
 
-	/**
-	 * 发送极光推送通知给所有用户
-	 * 
-	 * @param content
-	 */
-	public PushResult sendNotificationAll(String content) throws APIConnectionException, APIRequestException {
-		PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.all()).setNotification(Notification.alert(content)).build();
-		PushResult result = sendPush(payload);
-		return result;
-	}
+    /**
+     * 发送极光推送通知给所有用户
+     *
+     * @param content
+     */
+    public PushResult sendNotificationAll(String content) throws APIConnectionException, APIRequestException {
+        PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all()).setAudience(Audience.all()).setNotification(Notification.alert(content)).build();
+        PushResult result = sendPush(payload);
+        return result;
+    }
 
-	private PushResult sendPush(PushPayload payload) throws APIConnectionException, APIRequestException {
-		PushResult result = client.sendPush(payload);
-		logger.info("发送极光推送返回结果:" + result.toString());
-		return result;
-	}
+
+    /**
+     * 使用别名发送极光推送消息
+     *
+     * @param user
+     * @param content
+     */
+    public PushResult sendMessage(String user, String content) throws APIConnectionException, APIRequestException {
+        PushPayload payload = PushPayload.newBuilder()
+                .setPlatform(Platform.all())
+                .setAudience(Audience.alias(user))
+                .setMessage(Message.newBuilder().setMsgContent(content).build())
+                .build();
+        return sendPush(payload);
+    }
+
+    /**
+     * 使用别名列表发送极光推送消息
+     *
+     * @param users
+     * @param content
+     * @throws APIRequestException
+     * @throws APIConnectionException
+     */
+    public PushResult sendMessage(List<String> users, String content) throws APIConnectionException, APIRequestException {
+        PushPayload payload = PushPayload.newBuilder()
+                .setPlatform(Platform.all())
+                .setAudience(Audience.newBuilder().addAudienceTarget(AudienceTarget.alias(users)).build())
+                .setMessage(Message.newBuilder().setMsgContent(content).build())
+                .build();
+        return sendPush(payload);
+    }
+
+    private PushResult sendPush(PushPayload payload) throws APIConnectionException, APIRequestException {
+        PushResult result = client.sendPush(payload);
+        logger.info("发送极光推送返回结果:" + result.toString());
+        return result;
+    }
 }

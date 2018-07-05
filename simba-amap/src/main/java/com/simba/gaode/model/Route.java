e086db102c40644dd35916cde08d8261cdc97758
loceme.student@163.com <loceme.student@163.com>
2018-07-05 09:51:37 +0800

    高德地图Web Api java版本封装SDK

--- simba-amap/src/main/java/com/simba/gaode/model/Route.java
+++ simba-amap/src/main/java/com/simba/gaode/model/Route.java
@@ -1,5 +1,7 @@
 package com.simba.gaode.model;
 
+import java.util.List;
+
 /**
  * 路线信息
  * 
@@ -7,12 +9,57 @@ package com.simba.gaode.model;
  *
  */
 public class Route {
-	
+
 	/**
 	 * 起点坐标
 	 */
 	private String origin;
-	
-	
+
+	/**
+	 * 终点坐标
+	 */
+	private String destination;
+
+	/**
+	 * 路线方案
+	 */
+	private List<Path> paths;
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public List<Path> getPaths() {
+		return paths;
+	}
+
+	public void setPaths(List<Path> paths) {
+		this.paths = paths;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Route [origin=");
+		builder.append(origin);
+		builder.append(", destination=");
+		builder.append(destination);
+		builder.append(", paths=");
+		builder.append(paths);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
---
+++ simba-amap/src/main/java/com/simba/gaode/model/Step.java
@@ -0,0 +1,33 @@
+package com.simba.gaode.model;
+
+/**
+ * 每段方案
+ * 
+ * @author caozhejun
+ *
+ */
+public class Step {
+
+	/**
+	 * 路段指示
+	 */
+	private String instruction;
+
+	/**
+	 * 道路名称
+	 */
+	private String road;
+
+	/**
+	 * 此路段距离
+	 * 
+	 * 单位：米
+	 */
+	private String distance;
+	
+	/**
+	 * 
+	 */
+	private String orientation;
+
+}

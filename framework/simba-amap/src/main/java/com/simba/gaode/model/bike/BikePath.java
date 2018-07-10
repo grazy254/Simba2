639ca7c90ea604579cdc03c77e6b550612365f22
loceme <loceme.student@163.com>
2018-07-10 10:46:42 +0800

    高德地图web api java版本封装

--- framework/simba-amap/src/main/java/com/simba/gaode/model/bike/BikePath.java
+++ framework/simba-amap/src/main/java/com/simba/gaode/model/bike/BikePath.java
@@ -1,5 +1,9 @@
 package com.simba.gaode.model.bike;
 
+import java.util.List;
+
+import com.simba.gaode.model.Step;
+
 /**
  * 骑行方案列表信息
  * 
@@ -7,5 +11,57 @@ package com.simba.gaode.model.bike;
  *
  */
 public class BikePath {
+
+	/**
+	 * 起终点的骑行距离 单位：米
+	 */
+	private String distance;
+
+	/**
+	 * 起终点的骑行时间 单位：秒
+	 */
+	private String duration;
+
+	/**
+	 * 具体骑行结果
+	 */
+	private List<Step> steps;
+
+	public String getDistance() {
+		return distance;
+	}
+
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public List<Step> getSteps() {
+		return steps;
+	}
+
+	public void setSteps(List<Step> steps) {
+		this.steps = steps;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("BikePath [distance=");
+		builder.append(distance);
+		builder.append(", duration=");
+		builder.append(duration);
+		builder.append(", steps=");
+		builder.append(steps);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }

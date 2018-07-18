d02b4309b3c7ac6e7d0f7a95fbecbc6b544559ce
loceme.student@163.com <loceme.student@163.com>
2018-07-18 17:24:13 +0800

    okhttp封装

--- framework/simba-okhttp/src/main/java/com/simba/okhttp/util/OkHttpClientUtil.java
+++ framework/simba-okhttp/src/main/java/com/simba/okhttp/util/OkHttpClientUtil.java
@@ -1,6 +1,7 @@
 package com.simba.okhttp.util;
 
 import java.io.IOException;
+import java.util.Map;
 
 import com.simba.exception.BussException;
 
@@ -83,4 +84,58 @@ public class OkHttpClientUtil {
 		asyncExecute(request, callback);
 	}
 
+	/**
+	 * 同步发送http请求
+	 * 
+	 * @param url
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 */
+	public String get(String url, Map<String, String> params) throws IOException {
+		url = buildUrl(url, params);
+		return get(url);
+	}
+
+	/**
+	 * 异步发送http请求
+	 * 
+	 * @param url
+	 * @param params
+	 * @param callback
+	 */
+	public void get(String url, Map<String, String> params, Callback callback) {
+		url = buildUrl(url, params);
+		get(url, callback);
+	}
+
+	/**
+	 * 将参数拼接到url后
+	 * 
+	 * @param url
+	 * @param params
+	 * @return
+	 */
+	private String buildUrl(String url, Map<String, String> params) {
+		StringBuilder queryString = new StringBuilder(url);
+		if (url.indexOf("?") > -1) {
+			queryString.append("&");
+		} else {
+			queryString.append("?");
+		}
+		int index = 0;
+		int size = params.size();
+		for (Map.Entry<String, String> entry : params.entrySet()) {
+			queryString.append(entry.getKey() + "=" + entry.getValue());
+			if (index != size - 1) {
+				queryString.append("&");
+			}
+			index++;
+		}
+		return queryString.toString();
+	}
+
+	public String post(String url) {
+		return null;
+	}
 }

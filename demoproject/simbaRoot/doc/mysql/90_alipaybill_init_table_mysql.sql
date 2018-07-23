53b30e965a09c9dad76539b4f3960f95054ec6f0
loceme <loceme.student@163.com>
2018-07-23 09:18:33 +0800

    支付

--- demoproject/simbaRoot/doc/mysql/90_alipaybill_init_table_mysql.sql
+++ demoproject/simbaRoot/doc/mysql/90_alipaybill_init_table_mysql.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/5/25 星期五 14:39:29                       */
+/* Created on:     2018/7/23 星期一 9:18:10                        */
 /*==============================================================*/
 
 
@@ -25,6 +25,17 @@ create table aliPayBill
    timeoutExpress       varchar(16) comment '最晚付款时间',
    status               varchar(64) comment '状态',
    createTime           datetime comment '订单时间',
+   outBizNo             varchar(64) comment '商户业务ID',
+   buyerId              varchar(64) comment '买家支付宝账号对应的支付宝唯一用户号',
+   buyerLogonId         varchar(64) comment '买家支付宝账号',
+   sellerEmail          varchar(128) comment '卖家支付宝账号',
+   receiptAmount        varchar(9) comment '商家在交易中实际收到的款项',
+   invoiceAmount        varchar(9) comment '用户在交易中支付的可开发票的金额',
+   buyerPayAmount       varchar(9) comment '用户在交易中支付的金额',
+   refundFee            varchar(9) comment '退款通知中，返回总退款金额',
+   payTime              varchar(32) comment '买家付款时间',
+   refundTime           varchar(32) comment '退款时间',
+   closeTime            varchar(32) comment '结束时间',
    primary key (id)
 );
 

026e7d51fda13a1c535f26fe094b299b014c8d54
shuoGG <328893769@qq.com>
2018-07-26 10:13:25 +0800

    add pushCenter-sdk

---
+++ framework/simba-pushCenter-sdk/main/java/com/simba/arg/EmailArg.java
@@ -0,0 +1,25 @@
+package com.simba.arg;
+
+/**
+ * Created by shuoGG on 2018/7/25
+ */
+public class EmailArg {
+    private String subject;
+    private String content;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}

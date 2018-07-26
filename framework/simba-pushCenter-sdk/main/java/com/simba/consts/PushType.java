63b55e6b66734447358eb74c14d2f4935077a36c
shuoGG <328893769@qq.com>
2018-07-26 10:53:55 +0800

    pushcenter fix dependences

--- framework/simba-pushCenter-sdk/main/java/com/simba/consts/PushType.java
+++ framework/simba-pushCenter-sdk/main/java/com/simba/consts/PushType.java
@@ -4,9 +4,9 @@ package com.simba.consts;
  * Created by shuoGG on 2018/7/24
  */
 public class PushType {
-    public static final String JPUSH_NOTIFICATION = "JPUSH_NOTIFICATION";
-    public static final String JPUSH_MESSAGE = "JPUSH_MESSAGE";
-    public static final String WEBSOCKET =   "WEBSOCKET";
-    public static final String SHORT_MESSAGE = "SHORT_MESSAGE";
-    public static final String EMAIL = "EMAIL";
+    public static final String JPUSH_NOTIFICATION = "PUSH_CENTER_JPUSH_NOTIFICATION";
+    public static final String JPUSH_MESSAGE = "PUSH_CENTER_JPUSH_MESSAGE";
+    public static final String WEBSOCKET =   "PUSH_CENTER_WEBSOCKET";
+    public static final String SHORT_MESSAGE = "PUSH_CENTER_SHORT_MESSAGE";
+    public static final String EMAIL = "PUSH_CENTER_EMAIL";
 }

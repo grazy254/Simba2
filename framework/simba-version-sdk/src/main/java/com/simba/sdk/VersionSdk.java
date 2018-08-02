6c5086dff5cc97ea3cdd4300281417c699036fa8
loceme.student@163.com <loceme.student@163.com>
2018-08-02 14:15:56 +0800

    版本管理sdk

--- framework/simba-version-sdk/src/main/java/com/simba/sdk/VersionSdk.java
+++ framework/simba-version-sdk/src/main/java/com/simba/sdk/VersionSdk.java
@@ -1,5 +1,8 @@
 package com.simba.sdk;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +25,7 @@ public class VersionSdk {
 
 	private static final String getVersionUrl = "/VERSIONUSER/api/app/getVersionInfo";
 
-	private static final String saveBugFeedbackUrl = "/VERSIONUSER";
+	private static final String saveBugFeedbackUrl = "/VERSIONUSER/api/bugFeedback/save";
 
 	@Autowired
 	private EurekaClientUtil client;
@@ -31,6 +34,7 @@ public class VersionSdk {
 	 * 获取版本信息
 	 * 
 	 * @param typeId
+	 *            类型id
 	 * @return
 	 */
 	public JsonResult getVersionInfo(int typeId) {
@@ -38,6 +42,31 @@ public class VersionSdk {
 		String res = client.get(getUrl);
 		JsonResult result = FastJsonUtil.toObject(res, JsonResult.class);
 		result.check("获取版本信息发生异常");
+		return result;
+	}
+
+	/**
+	 * 保存bug反馈
+	 * 
+	 * @param userId
+	 *            反馈bug的用户id
+	 * @param title
+	 *            bug名称
+	 * @param text
+	 *            bug内容
+	 * @param img
+	 *            bug截图
+	 * @return
+	 */
+	public JsonResult saveBugFeedback(Integer userId, String title, String text, String img) {
+		Map<String, String> param = new HashMap<>();
+		param.put("userId", userId + "");
+		param.put("title", title);
+		param.put("text", text);
+		param.put("img", img);
+		String res = client.post(saveBugFeedbackUrl, param);
+		JsonResult result = FastJsonUtil.toObject(res, JsonResult.class);
+		result.check("保存bug反馈发生异常");
 		return result;
 	}
 }

ca3c7dade1019afaa4f41e77053de8dc53867ac3
曹哲军 <caozhejun@ut.cn>
2018-09-29 16:23:02 +0800

    微服务组件

--- demoproject/simbaRoot/doc/mysql/3_init_registry_mysql.sql
+++ demoproject/simbaRoot/doc/mysql/3_init_registry_mysql.sql
@@ -7,7 +7,7 @@ insert into registryType(id,text,parentID) values(7,'钱包管理',-1);
 insert into registryType(id,text,parentID) values(8,'交易部门',7);
 insert into registryType(id,text,parentID) values(9,'交易渠道',7);
 insert into registryTable(id,code,value,typeID,description) values(2,'alarm.exception.email.enabled','true',2,'是否开启异常邮件提醒');
-insert into registryTable(id,code,value,typeID,description) values(3,'alarm.email','caozhejun@ut.cn',2,'接收异常邮件的邮箱地址，多个使用英文逗号隔开');
+insert into registryTable(id,code,value,typeID,description) values(3,'alarm.email','loceme.student@163.com',2,'接收异常邮件的邮箱地址，多个使用英文逗号隔开');
 insert into registryTable(id,code,value,typeID,description) values(4,'alarm.exception','all',2,'需要发送异常邮件提醒的异常类全路径，多个使用英文逗号隔开');
 insert into registryTable(id,code,value,typeID,description) values(5,'alarm.exclude.exception','none',2,'不需要发送异常邮件提醒的异常类全路径，多个使用英文逗号隔开');
 insert into registryTable(id,code,value,typeID,description) values(6,'autoDeployKey','simbaAutoDeploy',3,'自动化部署提交部署文件时的秘钥');
